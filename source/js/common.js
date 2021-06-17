9dbfd5fee1ef2618fd5b4bfd3132f20678941234
Andrey <soinproduction@gmail.com>
2021-06-17 14:38:32 +0300

    create modal

--- source/js/common.js
+++ source/js/common.js
@@ -7,14 +7,25 @@ document.getElementById('close').onclick = function() {
   document.getElementById('burger-btn').classList.remove('burger-btn--active');
   document.getElementById('header-menu').classList.remove('header-menu--active');
 }
-const anch = document.querySelectorAll("a[href*='#']")
 
+document.getElementById('reg-code').onclick = function() {
+  document.getElementById('overlay').classList.toggle('overlay--active');
+  document.getElementById('modal').classList.toggle('modal--active');
+}
+
+document.getElementById('close-btn').onclick = function() {
+  document.getElementById('overlay').classList.remove('overlay--active');
+  document.getElementById('modal').classList.remove('modal--active');
+}
+
+
+const anch = document.querySelectorAll("a[href*='#']")
 
 for (let anchs of anch ) {
   anchs.addEventListener('click', function(event) {
     event.preventDefault();
     const blockId = anchs.getAttribute('href')
-    document.querySelector('' + blockId).scrollIntoView({
+    document.querySelector('section' + blockId).scrollIntoView({
       behavior: "smooth",
       block: "start"
     })
