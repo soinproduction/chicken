9f39a056370afe0af1bf3d4f1e3639b2fbf56e39
Andrey <soinproduction@gmail.com>
2021-06-18 13:24:02 +0300

    create media

--- gulpfile.js
+++ gulpfile.js
@@ -114,7 +114,8 @@ gulp.task("copy", () => {
   return gulp.src([
       "source/fonts/**/*.{woff,woff2}",
       "source/img/**/*.{png,jpg,jpeg,svg,gif}",
-      "source/*.ico"
+      "source/*.ico",
+      "source/*.pdf"
     ], {
       base: "source"
     })
